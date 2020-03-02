6ddea48213eaa06a84cf682a5c43478c06c02aaa
Fernando <Fernando@Fernando>
2020-03-01 20:58:21 -0600

    Commit inicial con eclipse tool suite 4

---
+++ f-shop/src/main/java/com/fernando/fshop/FShopApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.fshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FShopApplication.class, args);
+	}
+
+}
